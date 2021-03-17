91565ca88c2bc3a4fdcb4cfd0b50debdb89bd095
Katia <morenokatiac@gmail.com>
2021-03-17 20:04:06 +0000

    km-portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// jQuery code to show the working of this method 
+    $(document).ready(function() { 
+        $("button").click(function() { 
+            $(document).scrollTop($(document).height()); 
+        }); 
+    }); 
+
