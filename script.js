5889a8355a634a6c6905dab170823c4df8c13606
Katia <morenokatiac@gmail.com>
2021-03-20 15:47:24 +0000

    update

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // jQuery code to show the working of this method 
-    $(document).ready(function() { 
-        $("button").click(function() { 
-            $(document).scrollTop($(document).height()); 
-        }); 
-    }); 
+$(document).ready(function () {
+    $("button").click(function () {
+        $(document).scrollTop($(document).height());
+    });
+});
 
+var broken = false
+function workLamp(tipo) {
+    if(!broken){
+        document.getElementById("luz").src = "images/lamp/" + tipo + ".png"
+       if (tipo == 'lampada-quebrada-removebg-preview'){
+        quebrada = true
+       }
+    }
+}
